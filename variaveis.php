68b3a4b48e641b19abed76763bcbd449c315b99c
Eduardo <thiago.novak7@gmail.com>
2020-08-10 21:01:36 -0300

    first commit

---
+++ variaveis.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variaveis</title>
+</head>
+<body>
+    <?php
+     $idade = 28;
+     $nome = (string)"Eduardo";   
+     //echo $nome;
+     
+     
+
+     //concatenação
+     echo $nome . " tem " . $idade. " anos "
+
+    
+    ?>
+
+</body>
+</html>
