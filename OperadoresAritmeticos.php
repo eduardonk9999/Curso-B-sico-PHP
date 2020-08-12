ac93842a2edd80e9ecf0177d5e6c713944b5a327
Eduardo <thiago.novak7@gmail.com>
2020-08-12 09:31:55 -0300

    Add, Aritimeticos

--- OperadoresAritmeticos.php
+++ OperadoresAritmeticos.php
@@ -15,11 +15,13 @@
         DIVISÃO /
         MÓDULO %
         
-        
+
+        ABS () => PEGA  O VALOR ABSOLUTO DE UMA VARIAVEL
+         
         */
 
-        $n1 = 3;
-        $n2 = 2;
+        $n1 = $_GET["a"];
+        $n2 = $_GET["b"];
         $s = $n1 + $n2; 
     
         echo "A SOMA VALE ". $s;
